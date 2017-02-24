d875b53488c72995ad76d599cff5430d0318e3bd
BLFILLEIGH\sprince <sprince@brayleino.co.uk>
2017-02-24 11:55:49 +0000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+	plugins = require('gulp-load-plugins')(),
+
+	paths = {
+		scripts: {
+			src: ['./js/src/vendors/*.js', './js/src/components/*.js', './js/src/*.js'],
+			dist: './js/dist',
+			lint: ['./js/src/components/*.js', './js/src/*.js'],
+		},
+		sass: {
+			src: ['./scss/**/*.scss', '!./scss/amp.scss'],
+			dist: './css',
+			ampsrc: '.scss/amp.scss'
+		},
+		css: {
+			src: './css/style.css',
+			tmp: './tmp',
+		},
+		images: {
+			src: './img/src/**/*.{jpg,jpeg,png,gif,svg,PNG}',
+			dist: './img/'
+		}
+	},
+
+	config = {
+		autoprefixer: {
+			browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+		},
+		images: {
+			options: {
+		      optimizationLevel: 3,
+		      progessive: true,
+		      interlaced: true
+		    }
+		}
+	};
+
+function getTask(task) {
+    return require('./gulp-tasks/' + task)(config, paths, gulp, plugins);
+}
+
+gulp.task('scripts-dev', getTask('scripts-dev'));
+gulp.task('scripts-build', getTask('scripts-build'));
+gulp.task('scripts-lint', getTask('scripts-lint'));
+gulp.task('sass-dev', getTask('sass-dev'));
+gulp.task('sass-build', getTask('sass-build'));
+gulp.task('amp-css', getTask('amp-css'));
+gulp.task('amp-build', getTask('amp-build'));
+gulp.task('imgmin', getTask('imgmin'));
+
+gulp.task('amp', ['amp-css', 'amp-build']);
+
+gulp.task('watch', function () {
+	gulp.watch(paths.sass.src, ['sass-dev']);
+	gulp.watch(paths.scripts.src, ['scripts-dev']);
+});
+
+gulp.task('build', ['sass-build', 'scripts-build', 'imgmin', 'amp-css']);
